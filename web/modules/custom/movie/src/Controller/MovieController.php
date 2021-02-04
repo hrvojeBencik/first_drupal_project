d9fe2ec41e560cb8af7d50e2705089c310f1f2dc
hrvojeBencik <hrvojebencik1@gmail.com>
2021-02-04 13:54:32 +0100

    #20-Added-listing-movies-from-genre

--- web/modules/custom/movie/src/Controller/MovieController.php
+++ web/modules/custom/movie/src/Controller/MovieController.php
@@ -5,11 +5,18 @@ namespace Drupal\movie\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 class MovieController extends  ControllerBase {
-  public function movie_content() {
 
+  public function getMovies() {
     $node = \Drupal::entityTypeManager()->getStorage('node');
     $nids = $node->getQuery()->condition('status', 1)->condition('type', 'Movie')->execute();
     $movies = $node->loadMultiple($nids);
+
+    return $movies;
+  }
+
+  public function movie_content() {
+
+    $movies = $this->getMovies();
 
     return [
       '#theme' => 'movie_theme_hook',
@@ -18,10 +25,10 @@ class MovieController extends  ControllerBase {
   }
 
   public function movie_reservation_content() {
-    $vocabulary = 'Genres';
-    $node = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
-    $terms = $node->loadTree($vocabulary);
 
+    $vocabulary = 'Genres';
+    $taxonomy = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
+    $terms = $taxonomy->loadTree($vocabulary);
     foreach ($terms as $term) {
       $genres_data[] = array(
         'id' => $term->tid,
@@ -29,10 +36,15 @@ class MovieController extends  ControllerBase {
       );
     }
 
+    $selectedGenre = \Drupal::request()->request->get('genres') ? \Drupal::request()->request->get('genres') : 'Action';
+
+    $movies = $this->getMovies();
+
     return [
       '#theme' => 'movie_reservation_theme_hook',
+      '#movies' => $movies,
       '#genres' => $genres_data,
+      '#selectedGenre' => $selectedGenre,
     ];
   }
-
 }
